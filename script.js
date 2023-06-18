9c9f83eda09b16670002b4bb925be0740edaa2fe
SinGer ZS <singer.nzs@gmail.com>
2023-06-18 14:09:09 -0400

    Ticket #U3_00

--- script.js
+++ script.js
@@ -14,14 +14,14 @@
 */
 
 export const gameDetails = {   
-    title: 'Game Title',
-    desc: 'Welcome to the world of... here are some quick rules & concepts...',
-    author: 'Student Name',
-    cohort: 'SBPT-2022',
-    startingRoomDescription: 'What you see before you is...',
+    title: 'Walking in the Park',
+    desc: "Welcome to the world of Nowhere! We're happy to have you visit with us, and hope you have a safe time exploring our local park. Fun time! I mean, it'll of course be safe AND fun. Now, while we're sure you understand the basic concept of walking, our undying hospitable nature demands we show our new tourists the ropes. (Metaphorically, of course!) So, here are some helpful tips that will make your time with us a truly undeniably, unforgettably, unmatched experience. FIRST! In this place, you are only free to explore ahead or behind--this means, that to return to a previous area will require some memory. Do your best to follow the maze, er...I mean path, and you will be fine! SECOND!! To interact with the world or to choose a path, be sure you speak clearly so our guide can help you along. This means reiterating key verbs or phrases that a local Nowhereian would understand. You can identify these words by their LOUD EXPRESSION! We Nowhereians are a boisterous bunch. FINALLY, THIRD! Don't forget to explore each new area and gather as many clues as possible. You never know what mysterious...I mean curious...no, I mean INTERESTING things you might happen upon in the dungeon..gah! No! In the Park! We're just walking in the Park! It's all normal! We're all fine. You're good. Okay, bye!",
+    author: 'SinGer',
+    cohort: 'SBPT-2023',
+    startingRoomDescription: "What you see before you is a large iron gate that is tightly shut with a padlock. Above the gate, there is a sign that reads 'PARK'. To your left and right there are whitewashed stone walls with ivy poking above and throughout them. On the ground, paralell to either wall, is a worn sidewalk covered in dirt and litter with what appear to be arrows pointing East in one direction, or West the other.",
     playerCommands: [
         // replace these with your games commands as needed
-        'inspect', 'view', 'look', 'pickup',
+        'inspect', 'view', 'look', 'pickup', 'enter', 'break', 'climb'
     ]
     // Commands are basic things that a player can do throughout the game besides possibly moving to another room. This line will populate on the footer of your game for players to reference. 
     // This shouldn't be more than 6-8 different commands.
